9804daf409f248c2fe355072a2210b19e7a843f0
Rsja12 <asocarras10@gmail.com>
2020-05-13 14:40:35 -0400

    Auth0

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import NavBar from './NavBar'
 import QuestionsIndex from './questions/QuestionsIndex'
 import QuestionShow from './questions/QuestionShow'
+import { Callback } from './Callback'
 
 const App = () => {
     return (
@@ -11,6 +12,7 @@ const App = () => {
             <NavBar />
             <Route exact path='/' component={QuestionsIndex} />
             <Route exact path='/question/:id' component={QuestionShow} />
+            <Route exact path='/callback' component={Callback} />
         </div>
     )
 }
--- src/authentication/Auth.js
+++ src/authentication/Auth.js
@@ -1,13 +1,66 @@
-import React, { Component } from 'react'
+import auth0 from 'auth0-js'
 
-export class Auth extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+class Auth {
+
+    constructor() {
+        this.auth0 = new auth0.WebAuth({
+            domain: `${process.env.REACT_APP_DOMAIN}`,
+            audience: `https://${process.env.REACT_APP_DOMAIN}/userinfo`,
+            clientID: `${process.env.REACT_APP_CLIENT_ID}`,
+            redirectUri: 'http://localhost:3000/callback',
+            responseType: 'id_token',
+            scope: 'openid profile'
+        })
+
+        this.getProfile = this.getProfile.bind(this);
+        this.handleAuthentication = this.handleAuthentication.bind(this);
+        this.isAuthenticated = this.isAuthenticated.bind(this);
+        this.signIn = this.signIn.bind(this);
+        this.signOut = this.signOut.bind(this);
+
     }
+
+    getProfile() {
+        return this.profile;
+    }
+    
+    getIdToken() {
+        return this.idToken;
+    }
+
+    isAuthenticated() {
+        return new Date().getTime() < this.expiresAt;
+    }
+
+    handleAuthentication() {
+        return new Promise((resolve, reject) => {
+            this.auth0.parseHash((err, authResult) => {
+                if (err) return reject(err);
+                if (!authResult || !authResult.idToken) {
+                    return reject(err);
+                }
+                this.idToken = authResult.idToken;
+                this.profile = authResult.idTokenPayload;
+                // set the time that the id token will expire at
+                this.expiresAt = authResult.idTokenPayload.exp * 1000;
+                resolve();
+            });
+        })
+    }
+
+    signIn() {
+        this.auth0.authorize();
+    }
+
+    signOut() {
+    // clear id token, profile, and expiration
+        this.idToken = null;
+        this.profile = null;
+        this.expiresAt = null;
+    }
+
 }
 
-export default Auth
+const auth0Client = new Auth()
+
+export default auth0Client
