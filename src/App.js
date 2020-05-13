42413a78b5493e6072467cdefc65a0867218b74a
Rsja12 <asocarras10@gmail.com>
2020-05-13 12:10:48 -0400

    create QuestionShow class component, Add Route components inside App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import NavBar from './NavBar'
 import QuestionsIndex from './Questions/QuestionsIndex'
+import QuestionShow from './Questions/QuestionShow'
 
 const App = () => {
     return (
         <div>
             <NavBar />
-            <QuestionsIndex />
+            <Route exact path='/' component={QuestionsIndex} />
+            <Route exact path='/question/:id' component={QuestionShow} />
         </div>
     )
 }
---
+++ src/Questions/QuestionShow.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class QuestionShow extends Component {
+
+    state = {
+        question: null
+    }
+
+    render() {
+        return (
+            <div>
+                QuestionShow
+            </div>
+        )
+    }
+}
+
+export default QuestionShow
