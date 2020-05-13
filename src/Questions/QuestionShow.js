4d95dce25a9b00c196f8a2d21f39d4ee49bd2940
Rsja12 <asocarras10@gmail.com>
2020-05-13 12:28:22 -0400

    refactor QuestionShow

--- src/Questions/QuestionShow.js
+++ src/Questions/QuestionShow.js
@@ -7,10 +7,48 @@ export class QuestionShow extends Component {
         question: null
     }
 
+    async componentDidMount() {
+        const { params } = this.props.match
+        const question = ( await axios.get(`http://localhost:8081/${params.id}`) ).data
+
+        this.setState({
+            question
+        })
+    }
+
+    renderAnswers() {
+        const { question } = this.state
+        return question.answers.map( answer => (
+            <p className='lead' key={answer.answer}></p>
+        ) )
+    }
+
+    renderQuestion() {
+        const { question } = this.state
+        return(
+            <div className='container'>
+                <div className='row'>
+                    <div className='jumbotron col-12'>
+                        <h1 className='display-3'>{ question.title }</h1>
+                        <p className='lead'>{ question.description }</p>
+                        <hr className='my-4' />
+                        <p>Answers:</p>
+                        { this.renderAnswers() }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
+        console.log(this.props)
+        const { question } = this.state
+        if ( !question ) return <div>Loading...</div>
+
+        console.log(question)
         return (
             <div>
-                QuestionShow
+                { this.renderQuestion() }
             </div>
         )
     }
