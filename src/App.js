101461aea0cf79599279954713801f24f255f0b9
Rsja12 <asocarras10@gmail.com>
2020-05-13 10:50:07 -0400

    add styles dir

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import NavBar from './NavBar'
+
 const App = () => {
     return (
         <div>
+            <NavBar />
             <p>Work in progress</p>
         </div>
     )
